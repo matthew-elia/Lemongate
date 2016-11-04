4e530d892e1fa27293b8341665ba2d2382e3c95b
matthew-elia <matt.elia@gramercytech.com>
2016-11-03 22:41:50 -0400

    last refactor

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -19,7 +19,7 @@ angular.module('tigrisApp')
 	      $scope.user = authData;
 	      
 	      if (!authData) {
-	      	$state.go('portfolio');
+	      	$state.go('login');
 	      } else {
 	      	$state.go('inbox');
 	      }; 
